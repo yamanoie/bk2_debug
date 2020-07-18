147170a9fd8249efe89a0496386d866b768457d9
Yuki Yamamoto <yamanoieyamanoie@yahoo.co.jp>
2020-07-18 10:57:08 +0000

    ouyou kdai 2 finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,10 +7,12 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
     @book = Book.new(book_params)
+    @book.user_id = current_user.id
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
@@ -21,6 +23,9 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user.id != current_user.id
+      redirect_to books_path
+    end
   end
 
 
@@ -34,16 +39,16 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
-  get 'homes/about'
+  get 'home/about' => 'homes#about'
   resources :users,only: [:show,:index,:edit,:update]
   resources :books
-  
 end
